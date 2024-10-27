30065f77bf7eeb2df12f23667ec11ef6b3fc37d7
Jibril Kalifa <120446317+Jibrilkelifa@users.noreply.github.com>
2024-10-27 19:24:43 +0300

    Update functionality separating

---
+++ src/main/java/com/example/Incident/model/Update.java
@@ -0,0 +1,4 @@
+package com.example.Incident.model;
+
+public class Update {
+}
--- src/main/java/com/example/Incident/services/IncidentService.java
+++ src/main/java/com/example/Incident/services/IncidentService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class IncidentService {
     @Autowired
@@ -33,8 +35,12 @@ public class IncidentService {
         incidentRepository.deleteById(id);
     }
     public Incident getIncidentById(Long id) {
-        return incidentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Incident not found"));
+        Optional<Incident> incident = incidentRepository.findById(id);
+        if (incident.isPresent()) {
+            return incident.get();
+        } else {
+            throw new RuntimeException("Incident not found for ID: " + id);
+        }
     }
     public void deleteAllIncidents() {
         incidentRepository.deleteAll();
